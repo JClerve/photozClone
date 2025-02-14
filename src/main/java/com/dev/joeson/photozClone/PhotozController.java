086459b4fe5b67e057803bcbdecab136c8133ff4
JClerve <joesonclerve9@gmail.com>
2025-02-14 22:38:22 +0530

    Get the data from api

--- src/main/java/com/dev/joeson/photozClone/PhotozController.java
+++ src/main/java/com/dev/joeson/photozClone/PhotozController.java
@@ -1,13 +1,25 @@
 package com.dev.joeson.photozClone;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class PhotozController {
 
+    private List<Photo> db = List.of(new Photo("1","Hello.jpg"));
+
     @GetMapping("/")
     public String hello(){
         return "Hello Pirana";
+    }
+
+    @GetMapping("/photo")
+    public List<Photo> get(){
+        return db;
+
     }
 }
